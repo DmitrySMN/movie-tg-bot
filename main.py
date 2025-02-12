ec7ae640d2626555cb8eaea1a322c74005f2e2b9
dmtr <ddmtry064@gmail.com>
2025-02-12 09:59:07 +0300

    feat: add start message

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ import logging
 import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import CommandStart
+from aiogram import types
+from aiogram.enums import ParseMode
+from aiogram.utils import markdown
 
 load_dotenv()
 
+bot = Bot(token=os.getenv('BOT_TOKEN'), parse_mode=ParseMode.MARKDOWN_V2)
+dp = Dispatcher(storage=MemoryStorage())
+
+# premiers_button = InlineKeyboardButton(text="Список премьер", callback_data='callback')
+
+# favorites_button = InlineKeyboardButton(text="Избранные фильмы", callback_data='callback')
+
+# keyboard = InlineKeyboardMarkup().add(premiers_button, favorites_button)
+
+@dp.message(CommandStart())
+async def handle_start(message: types.Message):
+    await message.answer_photo(
+        photo = 'https://m.media-amazon.com/images/I/A1hBTf09UkL.jpg',
+        caption= f'{markdown.bold('🎥Все что вы хотели знать о кинемотографе')}'
+        )
+
 async def main():
-    bot = Bot(token=os.getenv('BOT_TOKEN'))
-    dp = Dispatcher(storage=MemoryStorage())
-    #dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == "__main__":
