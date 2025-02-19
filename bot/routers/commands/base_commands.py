05f6d39d469dd34b374b135c1c702cfce7cacc22
Dmitry Semenov <ddmtry064@gmail.com>
2025-02-19 19:06:00 +0300

    feat: add movie button click handling

--- bot/routers/commands/base_commands.py
+++ bot/routers/commands/base_commands.py
@@ -1,8 +1,10 @@
 from aiogram import types, Router, F
 from aiogram.utils import markdown
 from aiogram.filters import CommandStart, Command
+from bot.keyboards.movie_keyboard import get_movie_keyboard
 from bot.keyboards.start_keyboard import *
-from bot.templates.messages.base_messages import get_start_message
+from bot.templates.messages.base_messages import *
+from bot.services.movie_service import MovieService
 
 router = Router()
 
@@ -17,7 +19,10 @@ async def handle_start(message: types.Message):
 
 @router.message(F.text.startswith('inlinequery'))
 async def handle_premiers(message: types.Message):
-    await message.bot.send_message(chat_id=message.chat.id, text="Movie details")
+    movie_id=message.text.split(':')[1]
+    movie = MovieService.get_movie_by_id(movie_id)
+    # print(movie)
+    await message.bot.send_photo(chat_id=message.chat.id, photo=movie['posterUrl'], caption=get_movie_message(name=movie['nameRu'], year=movie['year'], genres=movie['genres'], rating=movie['ratingKinopoisk'], description=movie['description']), reply_markup=get_movie_keyboard())
     await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
 @router.message()
