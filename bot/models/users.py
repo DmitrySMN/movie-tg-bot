a5b604cf0e76fe9ccf87d10d3177a8feb8cda8e1
Dmitry Semenov <ddmtry064@gmail.com>
2025-02-21 10:14:31 +0300

    feat: implemet adding new users to the database

---
+++ bot/models/users.py
@@ -0,0 +1,13 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String, ARRAY
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    favorites = Column(ARRAY(Integer))
--- bot/routers/commands/base_commands.py
+++ bot/routers/commands/base_commands.py
@@ -1,22 +1,44 @@
+import os
 from aiogram import types, Router, F
-from aiogram.utils import markdown
 from aiogram.filters import CommandStart, Command
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from bot.keyboards.movie_keyboard import get_movie_keyboard
 from bot.keyboards.start_keyboard import *
+from bot.models.users import Users
 from bot.templates.messages.base_messages import *
 from bot.services.movie_service import MovieService
 
 router = Router()
 
+load_dotenv(dotenv_path='../../../.env')
+
+engine = create_engine(os.getenv('DATABASE_URI'))
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
 @router.message(F.text == "🏠 На главную")
 @router.message(CommandStart())
 async def handle_start(message: types.Message):
-    await message.answer_photo(
-        photo = 'https://i.pinimg.com/736x/20/43/f8/2043f8e7fe36c0c7fec8ecd5304724dc.jpg',
-        caption = get_start_message(),
-        reply_markup=get_start_inline_keyboard(),
-        )
-    await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    try:
+        user = session.query(Users).filter(Users.username == message.from_user.username).first()
+        if user is None:
+            new_user = Users(username=message.from_user.username, favorites=[])
+            session.add(new_user)
+            session.commit()
+            session.close()
+    except BaseException as err:
+        print(err)
+    finally:
+        session.close()
+        await message.answer_photo(
+            photo = 'https://i.pinimg.com/736x/20/43/f8/2043f8e7fe36c0c7fec8ecd5304724dc.jpg',
+            caption = get_start_message(),
+            reply_markup=get_start_inline_keyboard(),
+            )
+        await message.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
 
 
 @router.message(F.text.startswith('inlinequery'))
