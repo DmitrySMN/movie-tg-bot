791ae6832c656828366eaba99c1470292ca5f133
Dmitry Semenov <ddmtry064@gmail.com>
2025-03-07 12:08:45 +0300

    refactor: change similar movie request

--- bot/routers/callback_handlers/similar_button_handler.py
+++ bot/routers/callback_handlers/similar_button_handler.py
@@ -9,8 +9,8 @@ router = Router()
 async def similar_button_handler(callback: CallbackQuery):
     movie_title = callback.data.split(':')[1]
     movie_year = int(callback.data.split(':')[2])
-    similar_movies = MovieService.get_similar_movies(movie_title, movie_year)['result']
-    similar_movie_buttons = list(map(lambda m: InlineKeyboardButton(text=m['movie_name'], callback_data='similar-movie-button'), similar_movies))
+    similar_movies = MovieService.get_similar_movies(movie_title, movie_year)
+    similar_movie_buttons = list(map(lambda m: InlineKeyboardButton(text=m['title'], callback_data='similar-movie-button'), similar_movies))
     similar_movie_buttons.append(InlineKeyboardButton(text="↪️ Назад", callback_data=f'markup-back-button'))
     await callback.bot.edit_message_reply_markup(chat_id=callback.message.chat.id,
                                           message_id=callback.message.message_id,reply_markup=InlineKeyboardMarkup(inline_keyboard=[[m] for m in similar_movie_buttons]))
